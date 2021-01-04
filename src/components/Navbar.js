64f7ef2de0d23a8222b07b7cb3f162a93dcbdd0e
areye022 <areye022@gmail.com>
2021-01-04 11:51:58 -0800

    removed extra contact button, and adjusted logo sizing to create responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,13 @@
 import React, {useState} from "react";
 import { Link } from 'react-router-dom';
-import Button from './Button';
 import './Navbar.css';
 
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true)
  
     const handleClick =()=> setClick(!click);
     const closeMobileMenu=()=> setClick(false);
-
-    // to display
-    const showButton =() => {
-        if(window.innerWidth<=960) {
-            setButton(false)
-        }else{
-            setButton(true);
-        }
-    };
-    
-    // whenevr resizing, showButton will be prompted
-    window.addEventListener('resize', showButton);
 
     return (
         <div>
@@ -51,7 +37,6 @@ function Navbar() {
                                 </Link>
                             </li>
                         </ul>
-                        {button && <Button buttonStyle='btn--outline'> Contact </Button>}
                     </div>
                 </nav>
             </>
