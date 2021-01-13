df48c116e6edc085167fcf0be8277b41726aac85
areye022 <areye022@gmail.com>
2021-01-13 12:18:29 -0800

    created contact page, still unable to send emails, continue getting error message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import Navbar from "./components/Navbar"
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/pages/Home'
 import Portfolio from './components/pages/Portfolio'
 import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
         </Switch>
       </Router>
     </>
